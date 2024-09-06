793c27417a46fdc11de3d4dabc7f8c09ddeff12a
Dayys <161561501+dartyourt@users.noreply.github.com>
2024-09-06 02:53:47 +0000

    upload file

---
+++ WEEK 2/mtabel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "tabel.h"
+/ *Deskripsi : driver ADT tabel integer*/
+/* NIM/Nama : */
+/* Tanggal : */
+
+int main() {
+	/*kamus*/
+	
+	
+	/*algoritma*/
+	//createTable(&T1);
+	//getSize(T1);
+	//printf("%d", searchX(T,6));
+	//addXTable(&T1,6);
+	//printTable(T1);
+	//delXTable(&T1,6);
+	//printTable(T1);
+	//dst ...
+	return 0;
+}
