dcca07f850e159bad346b043d495692f3a31ddf8
Dayys <161561501+dartyourt@users.noreply.github.com>
2024-09-06 03:16:35 +0000

    update code

--- WEEK 2/mtabel.c
+++ WEEK 2/mtabel.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-#include "tabel.h"
-/ *Deskripsi : driver ADT tabel integer*/
-/* NIM/Nama : */
-/* Tanggal : */
+#include "tabel.c"
+/*Deskripsi : driver ADT tabel integer*/
+/* NIM/Nama : 24060123140151/Haidar Ali Laudza*/
+/* Tanggal  : 06/09/2024*/
 
 int main() {
 	/*kamus*/
+	Tabel T1;
 	
 	
 	/*algoritma*/
-	//createTable(&T1);
-	//getSize(T1);
-	//printf("%d", searchX(T,6));
-	//addXTable(&T1,6);
+	createTable(&T1);
+	getSize(T1);
+	addXTable(&T1,6);
+	addXTable(&T1,7);
+	addXTable(&T1,9);
+	addXTable(&T1,15);
+	printf("%d", searchX(T,6));
 	//printTable(T1);
 	//delXTable(&T1,6);
 	//printTable(T1);
