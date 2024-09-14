492c56095309c9f78c288b6a7eb951accd7a74fa
Dayys <161561501+dartyourt@users.noreply.github.com>
2024-09-14 14:04:15 +0000

    update code

--- WEEK 3/mmatriks.c
+++ WEEK 3/mmatriks.c
@@ -5,24 +5,29 @@
 /***********************************/
 
 #include <stdio.h>
-#include "matriks.c"
+#include "matriks.h"
 
 int main() {
 	/*kamus*/
-	Matriks M;
-	int baris, kolom;
+	Matriks M, M1, M2;
 	
 	/*algoritma*/
 	initMatriks(&M);
-	printf("%s\n", isFullMatriks(M) ? "penuh" : "kosong");
-	addX(&M, 1, 1, 1);
-	addX(&M, 12, 5, 2);
-	addX(&M, 3, 2, 3);
-	printMatriks(M);
-	viewMatriks(M);
-	delX(&M, 12);
-	printMatriks(M);
-	viewMatriks(M);
-	printf("baris = %d, kolom = %d\n", getNBaris(M), getNKolom(M));
+	//printf("%s\n", isFullMatriks(M) ? "penuh" : "kosong");
+	//addX(&M, 1, 1, 1);
+	//addX(&M, 12, 5, 2);
+	//addX(&M, 3, 2, 3);
+	//printMatriks(M);
+	//viewMatriks(M);
+	//delX(&M, 12);
+	//printMatriks(M);
+	//viewMatriks(M);
+	//printf("baris = %d, kolom = %d\n", getNBaris(M), getNKolom(M));
+	populateMatriks(&M1, 3, 3);
+	populateMatriks(&M2, 3, 3);
+	viewMatriks(M1);
+	printf("x ada %d\n", countX(M1, 2));
+	viewMatriks(addMatriks(M1, M2));
+	viewMatriks(subMatriks(M1, M2));
 	return 0;
 }
