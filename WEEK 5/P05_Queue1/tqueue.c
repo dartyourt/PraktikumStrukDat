a173d9bcdeb64275483e7050ec5efce8162ede24
Dayys <161561501+dartyourt@users.noreply.github.com>
2024-09-27 04:22:16 +0000

    update code

--- WEEK 5/P05_Queue1/tqueue.c
+++ WEEK 5/P05_Queue1/tqueue.c
@@ -129,19 +129,20 @@ void enqueue(tqueue *Q, char e){
 void dequeue(tqueue *Q, char *e){
     //kamus lokal
     int i;
-    //algoritma
-    if(!isEmptyQueue(*Q)){
-        if(isOneElement(*Q)){
-            *e = infoHead(*Q);
-            (*Q).head = 0;
-            (*Q).tail = 0;
-        }
-        else{
-            *e = infoHead(*Q);
-            for(i=1; i<=sizeQueue(*Q); i++){
+    // algoritma
+    if (!isEmptyQueue(*Q)){
+        *e = infoHead(*Q);
+        (*Q).wadah[tail(*Q)] = '-';
+        if (!isOneElement(*Q)){
+            for (i=1; i<=sizeQueue(*Q); i++) {
                 (*Q).wadah[i] = (*Q).wadah[i+1];
             }
             (*Q).tail--;
+        }
+        else{
+            (*Q).head = 0;
+            (*Q).tail = 0;
+            
         }
     }
 }
@@ -184,10 +185,13 @@ int maxlength(tqueue Q1, tqueue Q2){
 {F.S.: elemen wadah Q1 atau Q2 bertambah 1, bila belum penuh}
 {proses: menambah elemen wadah pada antrian terpendek dari Q1 atau Q2} */
 void enqueue2(tqueue *Q1, tqueue *Q2, char e){
-    if (sizeQueue(*Q1) < sizeQueue(*Q2))
-        enqueue(Q1, e);
-    else
-        enqueue(Q2, e);
+    if(!isFullQueue(*Q1)&&!isFullQueue(*Q2)){
+        if(sizeQueue(*Q1)<=sizeQueue(*Q2)){
+            enqueue(Q1,e);
+        }else{
+            enqueue(Q2,e);
+        }
+    }
 }
 
 /*procedure dequeue2( input/output Q1:tQueue, input/output Q2:tQueue, output e: character )
@@ -196,10 +200,13 @@ void enqueue2(tqueue *Q1, tqueue *Q2, char e){
 {proses: mengurangi elemen wadah antrian terpanjang Q1 atau Q2, semua elemen di belakang head digeser maju }
 {bila awalnya 1 elemen, maka Head dan Tail antrian menjadi 0 } */
 void dequeue2(tqueue *Q1, tqueue *Q2, char *e){
-    if (sizeQueue(*Q1) > sizeQueue(*Q2))
-        dequeue(Q1, e);
-    else
-        dequeue(Q2, e);
+    if(!isEmptyQueue(*Q1)&&!isEmptyQueue(*Q2)) {
+        if(sizeQueue(*Q1)>=sizeQueue(*Q2)){
+            dequeue(Q1,e);
+        }else{
+            dequeue(Q2,e);
+        }
+    }
 }
 
 
